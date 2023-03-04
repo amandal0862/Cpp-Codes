fee62bdb0f8077cbc2a589185bb28c1e719db47a
amandal0862 <amandal.eduu@gmail.com>
2023-03-04 12:30:11 +0530

    left view of tree

---
+++ Tree/LeftView.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include <queue>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node* insert(node* root, int d){
+    if(root == NULL){
+        root = new node(d);
+        return root;
+    }
+
+    if(d < root->data)
+        root->left = insert(root->left, d);
+    else
+        root->right = insert(root->right, d);
+
+    return root;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+    node* root = NULL;
+
+    while(n--){
+        int val;
+        cin>>val;
+        root = insert(root, val);
+    }
+
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        int s = q.size();
+
+        for(int i=1 ; i<=s ; i++){
+            node* d = q.front();
+            q.pop();
+
+            if(i==1){
+                cout<<d->data<<" ";
+            }
+
+            if(d->left != NULL){
+                q.push(d->left);
+            }
+
+            if(d->right != NULL){
+                q.push(d->right);
+            }
+        }
+    }
+}
